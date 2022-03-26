fe81488ca13490e0d26c1df4712cb175480eb864
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-03-26 13:15:32 +0100

    [B3-Character]
    [added] New CharacterModel
    [changed] Associated Character to Universe in UniverseModel
    [added] createCharacter, updateCharacter, findCharacter, findUniverseCharacters, searchCharacters, destroyCharacter functions in CharacterService
    [added] getCharacter, getUniverseCharacters, getSearchedCharacters, postCharacter, deleteCharacter, patchCharacter functions in CharacterController
    [added] New CharacterRouter
    [fixed] Using correct function in getSearchedCharacters in CharacterController

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import UserRouter from "./UserRouter.js";
 import UniverseRouter from "./UniverseRouter.js";
+import CharacterRouter from "./CharacterRouter.js";
 
 // define /api/* routers
 const mainRouter = new Router();
 mainRouter.use(UserRouter);
 mainRouter.use(UniverseRouter);
+mainRouter.use(CharacterRouter);
 // define /api router
 const apiRouter = new Router();
 apiRouter.use("/api", mainRouter);
---
+++ backend/services/CharacterService.js
@@ -0,0 +1,114 @@
+import { db } from '../models/index.js';
+import { NotFoundException } from "../errors.js";
+import { Sequelize } from "sequelize";
+
+async function createCharacter(name, description, universeID) {
+    let lastModified = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    try {
+        db.Character.create({
+            name: name,
+            description: description,
+            last_modified: lastModified,
+            Universe_id: universeID
+        });
+    } catch(err) {
+        throw new Error(err.message);
+    }
+};
+
+async function findCharacter(id) {
+    try {
+        const Character = db.Character.findOne({
+            where: {
+                id: id
+            }
+        });
+        return Character;
+    } catch(err) {
+        throw new NotFoundException("Character not found");
+    }
+};
+
+async function findUniverseCharacters(universeID, limit, offset) {
+    let findLimit = limit;
+    if(!limit) {
+        findLimit = 50;
+    }
+    let findOffset = offset;
+    if(!offset) {
+        findOffset = 0;
+    }
+    try {
+        const Characters = db.Character.findAll({
+            where: {
+                Universe_id: universeID,
+            },
+            offset: findOffset,
+            limit: findLimit,
+            order: [['last_modified', 'DESC']]
+        });
+        return Characters;
+    } catch(err) {
+        throw new NotFoundException("Characters for this universe were not found");
+    }
+};
+
+async function searchCharacters(universeID, expr, limit, offset) {
+    let findLimit = limit;
+    if(!limit) {
+        findLimit = 50;
+    }
+    let findOffset = offset;
+    if(!offset) {
+        findOffset = 0;
+    }
+    let searchLike = '.*'+expr+'.*';
+    let Op = Sequelize.Op;
+    try {
+        const Characters = db.Character.findAll({
+            where: {
+                [Op.and]: [
+                    {Universe_id: universeID},
+                    {name: {
+                        [Op.regexp]: searchLike
+                    }}
+                ]
+            },
+            offset: findOffset,
+            limit: findLimit,
+            order: [['last_modified', 'DESC']]
+        });
+        return Characters;
+    } catch(err) {
+        throw new Error(err.message);
+    }
+}
+
+async function destroyCharacter(id) {
+    try {
+        await db.Character.destroy({
+          where: { id: id },
+        });
+      } catch (err) {
+        throw new Error(e.message);
+      }
+};
+
+async function updateCharacter(id, updatedFields) {
+    let lastModified = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    try {
+        const modelKeys = Object.keys(db.Character.getAttributes());
+        let subsetFields = modelKeys
+            .filter((key) => key in updatedFields)
+            .reduce((subset, key) => {
+            subset[key] = updatedFields[key];
+            return subset;
+        }, {});
+        subsetFields.last_modified = lastModified;
+        await db.Character.update(subsetFields, { where: { id: id } })
+    } catch (err) {
+        throw new Error(e.message);
+    }
+};
+
+export { createCharacter, updateCharacter, findCharacter, findUniverseCharacters, searchCharacters, destroyCharacter };
