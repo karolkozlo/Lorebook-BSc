a5f4c4a21818b14d96075342d615d390624a5ade
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-07-09 18:02:53 +0200

    [F5-button-component]
    [added] LbButton component and used it as common component
    [refactored] Renamed few color names in common.less
    [added] Click emit in LbButton
    [fixed] Styling in LbButton

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
 //Common components
 import Icon from './components/Icon.vue';
+import LbButton from './components/LbButton.vue';
 
 const app = createApp(App).use(store).use(router);
 
 app.component("Icon", Icon);
+app.component("LbButton", LbButton);
 
 app.mount("#app");
 
