91cc1665997aab1ccec9f378273c9bcde86981ea
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-07-16 14:53:30 +0200

    [F10-PopupBox-component]
    [added] LbPopper component
    [added] LbPopupBox global component
    [changed] Adjusted styling in LbPopupBox
    [changed] Default content for empty slot
    [added] New prop message in LbPopupBox

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,13 @@ import store from './store';
 //Common components
 import Icon from './components/Icon.vue';
 import LbButton from './components/LbButton.vue';
+import LbPopupBox from './components/LbPopupBox.vue';
 
 const app = createApp(App).use(store).use(router);
 
 app.component("Icon", Icon);
 app.component("LbButton", LbButton);
+app.component("LbPopupBox", LbPopupBox);
 
 app.mount("#app");
 
