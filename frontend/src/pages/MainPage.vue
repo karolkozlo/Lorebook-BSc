0ad08aa2f27eb2520ca745485dc4c4209762e9cd
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-09-07 20:23:32 +0200

    [F50-menu-button-component]
    Added basic LbMenuButton component with data, props and methods
    Added styling to LbMenuButton
    Removed unnecessary computed property from LbButton
    Added use of LbMenuButton to MainPage for testing purposes

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -5,24 +5,54 @@
         <nav class="main-page__nav">
             <router-link to="/chapter"> Chapter </router-link>
         </nav>
+        <lb-menu-button icon="lb-plus" :size="1.2" :options="options" @click="clickOnMenuBtn">
+        </lb-menu-button>
     </div>
 </template>
 
 <script>
 import LbEditableText from '../components/LbEditableText.vue';
+import LbMenuButton from '../components/LbMenuButton.vue';
 export default {
     name: "MainPage",
     components: {
-      LbEditableText
+      LbEditableText,
+    LbMenuButton
     },
     data() {
         return {
-            text: 'Some text to edit'
+            text: 'Some text to edit',
+            options: [
+                {
+                    id: 1,
+                    name: 'Option 1',
+                    icon: 'lb-edit'
+                },
+                {
+                    id: 2,
+                    name: 'Option 2',
+                    icon: 'lb-link'
+                },
+                {
+                    id: 3,
+                    name: 'Option 3',
+                    icon: 'lb-list'
+                },
+                {
+                    id: 4,
+                    name: 'Option 4',
+                    icon: 'lb-picture'
+                }
+            ]
         };
     },
     methods: {
         saveText(newValue) {
             this.text = newValue;
+        },
+        clickOnMenuBtn(id) {
+            const option = this.options.find((o) => { return o.id == id });
+            console.log('Clicked option: ', option);
         }
     }
 };
