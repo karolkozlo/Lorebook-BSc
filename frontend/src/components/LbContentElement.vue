97900f1a9c740ad8622504c208016be44a625287
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-09-10 15:41:32 +0200

    [F4-content-element]
    Created basic LbContentElement with basic layout
    Added new common class for black line
    Added move buttons to LbContentElement
    Added test use of LbContentElement to MainPage
    Changed element color variable
    Fixed z-index problems in LbEditableText, LbPopper, LbSelect and LbMenuButton
    Added delete button and title field to LbEditableText
    Changed positioning of cancel and save button in LbEditableText
    Changed style of title in LbContentElement
    Changed size of buttons in LbContentElement
    Added handler for changing title in MainPage

---
+++ frontend/src/components/LbContentElement.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="lb-content-element">
+    <div class="lb-content-element__header">
+        <lb-editable-text :value="title"
+                          :maxLength="60"
+                          :width="'100%'"
+                          @onSave="changeTitle"
+                          customClass="lb-content-element__title">
+        </lb-editable-text>
+        <div class="lb-content-element__header-buttons">
+            <lb-button variant="negative" icon="lb-trash" @click="$emit('removeElement')" :size="1.2"></lb-button>
+        </div>
+    </div>
+    <div class="util__horizontal-line--black"></div>
+    <div class="lb-content-element__body">
+        <slot/>
+    </div>
+    <div class="lb-content-element__footer">
+        <div class="lb-content-element__footer-buttons">
+            <lb-button icon="lb-up" :disabled="isUpBtnDisabled" @click="moveContentElement(-1)" :size="1.1"></lb-button>
+            <lb-button icon="lb-down" :disabled="isLast" @click="moveContentElement(1)" :size="1.1"></lb-button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LbEditableText from './LbEditableText.vue';
+
+export default {
+    name: 'LbContentElement',
+    components: {
+        LbEditableText
+    },
+    emits: ['moveElement', 'removeElement', 'changeTitle'],
+    props: {
+        position: {
+            type: Number,
+            required: true,
+        },
+        isLast: {
+            type: Boolean,
+            default: false
+        },
+        title: {
+            type: String,
+            default: 'Title'
+        }
+    },
+    computed: {
+        isUpBtnDisabled() {
+            return this.position - 1 < 0;
+        }
+    },
+    methods: {
+        moveContentElement(change) {
+            this.$emit('moveElement', this.position + change);
+        },
+        changeTitle(newTitle) {
+            this.$emit('changeTitle', newTitle);
+        }
+    }
+};
+</script>
+
+<style lang="less">
+@import '../common.less';
+
+.lb-content-element {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    background-color: @element-color;
+    border-radius: 10px;
+    padding: 0.5em;
+    gap: 0.2em;
+
+    &:hover {
+        .lb-content-element__footer {
+            .lb-content-element__footer-buttons {
+                visibility: visible;
+            }
+        }
+
+        .lb-content-element__header {
+            .lb-content-element__header-buttons {
+                visibility: visible;
+            }
+        }
+    }
+
+    .lb-content-element__header {
+        display: flex;
+        justify-content: space-between;
+        gap: 10px;
+
+        .lb-content-element__title {
+            .lb-editable-text__text, .lb-editable-text__input {
+                font-size: 1.6rem;
+                font-weight: 500;
+            }
+        }
+
+        .lb-content-element__header-buttons {
+            visibility: hidden;
+        }
+    }
+
+    .lb-content-element__body {
+        display: flex;
+    }
+
+    .lb-content-element__footer {
+        display: flex;
+        justify-content: flex-end;
+
+        .lb-content-element__footer-buttons {
+            display: flex;
+            gap: 0.5em;
+            visibility: hidden;
+        }
+    }
+}
+
+</style>
