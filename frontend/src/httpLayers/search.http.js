822d4b3cf041adc345c8e5ab2847cf55cdf60722
karol.kozlo <karol.kozlowski1999@gmail.com>
2022-09-30 19:32:55 +0200

    [I15-integrate-search-page]
    Created getSearchedUniverseElements http function in search.http
    Created universeElementsMapper function in search.http
    Added loading state to SearchPage
    Added LbPageNav to SearchPage
    Added fetchElements, search and changePage methods to SearchPage
    Changed return of getSearchedUniverseElements function in search.http
    Removed unnecessary dangerous content from MainPage

--- frontend/src/httpLayers/search.http.js
+++ frontend/src/httpLayers/search.http.js
@@ -18,6 +18,68 @@ async function getUniverseElementsShortLists(universeID) {
         });
 };
 
+function universeElementsMapper(elements) {
+    return elements.map(el => {
+        return {
+            id: el.id,
+            name: el.name,
+            lastModified: el.last_modified,
+            categoryID: el.Category_id,
+            categoryName: el.categoryName
+        };
+    })
+};
+
+async function getSearchedUniverseElements(universeID, query, categories, tags, elementsPerPage, page) {
+    let queryParams = [];
+
+    if (categories && categories.length > 0) {
+        const encodedCategories = categories.map(cat => {
+            const encoded = encodeURIComponent(cat)
+            return `&categories=${encoded}`;
+        });
+        encodedCategories.forEach(cat => queryParams.push(cat));
+    }
+
+    if (tags && tags.length > 0) {
+        const encodedTags = tags.map(tag => {
+            const encoded = encodeURIComponent(tag)
+            return `&tags=${encoded}`;
+        });
+        encodedTags.forEach(tag => queryParams.push(tag));
+    }
+
+    let queryParamsString = '';
+    if (queryParams.length > 0) {
+        queryParams.forEach(qp => {
+            queryParamsString = `${queryParamsString}${qp}`;
+        });
+    }
+
+
+    const encodedQuery = encodeURIComponent(query ? query : '');
+    return await LbAPI
+        .get(`/search/${universeID}/?q=${encodedQuery}${queryParamsString}&limit=${elementsPerPage}&offset=${page}`)
+        .then((response) => {
+            const searchResult = {
+                elements: universeElementsMapper(response.data.rows),
+                totalPages: Math.ceil(response.data.count / elementsPerPage)
+            };
+            return searchResult;
+        })
+        .catch((error) => {
+            if (error.response && error.response.data.message) {
+                const err = new Error(`${error.response.data.message}`);
+                throw err;
+            } else if (error.request) {
+                throw new Error("Service refused connection");
+            } else {
+                throw new Error("Undefined error");
+            }
+        });
+};
+
 export {
-    getUniverseElementsShortLists
+    getUniverseElementsShortLists,
+    getSearchedUniverseElements
 }
--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -4,19 +4,7 @@
         <nav class="main-page__nav">
             <router-link to="/chapter"> Chapter </router-link>
         </nav>
-        <lb-menu-button icon="lb-plus" :size="1.2" :options="options" @click="clickOnMenuBtn">
-        </lb-menu-button>
         <div class="main-page__content">
-            <component v-for="(el, index) in contentElements"
-                       :is="componentType(el.type)"
-                       :key="{id: el.id, type: el.type}"
-                       :position="index"
-                       :isLast="isLast(index)"
-                       :contentID="contentID"
-                       v-bind="el"
-                       @moveElement="moveElement"
-                       @deleteElement="removeElement">
-            </component>
         </div>
     </div>
 </template>
@@ -42,118 +30,13 @@ export default {
     },
     data() {
         return {
-            contentID: 1,
-            contentElements: [
-                {
-                  id: 1,
-                  title: 'Some Title',
-                  text: 'Some starting text to edit in TextElement',
-                  type: contentElementType.text
-                },
-                {
-                  id: 2,
-                  title: 'Some Other Title',
-                  text: 'Some another starting text to edit in TextElement',
-                  type: contentElementType.text
-                },
-                {
-                  id: 1,
-                  title: 'Some Initial Title of List',
-                  type: contentElementType.list,
-                  items: [
-                    {
-                      id: 2,
-                      title: 'Another item with longer text',
-                      text: '',
-                      ordinalNumber: 0
-                    },
-                    {
-                      id: 3,
-                      title: 'item',
-                      text: 'dsf',
-                      ordinalNumber: 1
-                    },
-                  ]
-                },
-                {
-                    id: 1,
-                    title: 'Jakaś grupa linków',
-                    type: contentElementType.linkGroup,
-                    links: [
-                        {
-                            id: 1,
-                            description: 'Some description',
-                            targetName: 'Link do Lokacji',
-                            targetID: 1,
-                            categoryName: 'Locations',
-                            categoryID: 'Locations'
-                        },
-                        {
-                            id: 2,
-                            description: 'Ooopiss',
-                            targetName: 'Link do Stworzenia',
-                            targetID: 1,
-                            categoryName: 'Stworzenia',
-                            categoryID: 1
-                        },
-                    ]
-                }
-            ],
             text: 'Some text to edit',
-            options: [
-                {
-                    id: 1,
-                    name: 'Option 1',
-                    icon: 'lb-edit'
-                },
-                {
-                    id: 2,
-                    name: 'Option 2',
-                    icon: 'lb-link'
-                },
-                {
-                    id: 3,
-                    name: 'Option 3',
-                    icon: 'lb-list'
-                },
-                {
-                    id: 4,
-                    name: 'Option 4',
-                    icon: 'lb-picture'
-                }
-            ]
         };
     },
     methods: {
-        moveElement(positions) {
-            const el = this.contentElements.splice(positions.oldPosition, 1)[0];
-            this.contentElements.splice(positions.newPosition, 0, el);
-        },
-        removeElement(elToDel) {
-            this.contentElements = this.contentElements.filter(el => { return !(el.id == elToDel.id && el.type == elToDel.type); });
-            console.log('Remove element with id: '+elToDel.id+' and type of: '+elToDel.type);
-        },
         saveText(newValue) {
             this.text = newValue;
         },
-        clickOnMenuBtn(id) {
-            const option = this.options.find((o) => { return o.id == id });
-            console.log('Clicked option: ', option);
-            this.contentElements.push({
-                id: 3,
-                initTitle: 'New Element',
-                initText: 'Some new Element',
-                type: contentElementType.text
-            });
-        },
-        isLast(index) {
-            return this.contentElements.length-1 == index;
-        },
-        componentType(type) {
-            if (type == contentElementType.text) return 'LbTextElement';
-            if (type == contentElementType.list) return 'LbListElement';
-            if (type == contentElementType.linkGroup) return 'LbLinkGroupElement';
-        }
     }
 };
 </script>
